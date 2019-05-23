7b4e8d7046c3213c7d6e97e6f80cefa2841a9975
Burak Kaya <200383524@student.georgianc.on.ca>
2019-05-23 16:50:52 -0400

    Dynamic port assignment

--- app.js
+++ app.js
@@ -10,4 +10,8 @@ app.get("/about", (req, res) => {
   res.send("I like long walks on the beach.");
 });
 
-app.listen(4000, () => console.log("Listening on port 4000..."));
+const port = process.env.PORT || 4000;
+
+app.listen(process.env.PORT || 4000, () =>
+  console.log(`Listening on port ${port}...`)
+);
