9f499bbde0aa4c1c67749a2815ffc98f0199566e
Burak Kaya <200383524@student.georgianc.on.ca>
2019-05-30 18:39:52 -0400

    part of part3

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
+require("dotenv").config();
+
+// mongoose start
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MLAB_URI, {
+    auth: {
+      user: process.env.MLAB_USER,
+      password: process.env.MLAB_PASS
+    },
+    useNewUrlParser: true
+  })
+  .catch(err => console.error(`ERROR: ${err}`));
+// mongoose end
+
 const express = require("express");
 const path = require("path");
 
 const app = express();
 
+// Body parser
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+// End Parser
+
 // Our views path
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+app.use("/css", express.static("assets/styles"));
+app.use("/js", express.static("assets/scripts"));
+app.use("/images", express.static("assets/images"));
 // our routes
 
 const routes = require("./routes");
